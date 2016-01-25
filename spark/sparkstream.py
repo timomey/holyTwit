e44aecbb77af335abd6e8231e9ed662e650459e3
Timo Meyer <timomey@gmail.com>
2016-01-25 13:10:39 -0800

    cleaned the code, function go out, clean_string function added

--- spark/sparkstream.py
+++ spark/sparkstream.py
@@ -24,6 +24,53 @@ def define_the_search(word):
     #CREATE TABLE demostream (time text, city text PRIMARY KEY, country text);
     #""")
 
+def clean_string(text):
+    """input: string. output is a "clean" string:
+            1: spaces at end and beginning are removed:
+                                ' input ' -> 'input'
+            2: replacements:    "\/" -> "/"
+                                "\\" -> "\ "
+                                "\'" -> "'"
+                                '\"' -> '"'
+                                "\n" -> " ")
+                                "\t" -> " "
+            3: multiple spaces are replaced by one.
+                                "input      string" -> "input string"
+            """
+    text.strip()
+    text.replace("\/", "/")
+    text.replace("\\", "\ ")
+    text.replace("\'", "'")
+    text.replace('\"', '"')
+    text.replace("\n", " ")
+    text.replace("\t", " ")
+    text = " ".join(text.split())
+    text = text.lower()
+    return text
+
+def write_into_cassandra(record):
+    from cassandra.cluster import Cluster
+    from cassandra import ConsistencyLevel
+
+    # connect to cassandra
+    cluster = Cluster(['ec2-52-35-24-163.us-west-2.compute.amazonaws.com'])
+    session = cluster.connect("demo")
+
+    prepared_write_query = session.prepare("INSERT INTO demostream (time, city, country) VALUES (?,?,?)")
+    for i in record:
+        #print (i)
+        json_str = json.loads(i)
+        #print (json_str)
+
+        time = 'ttt'#str(json_str["created_at"])
+        city = 'test'#str(json_str["place"]["name"])
+        country = json_str #str(json_str["text"])
+        session.execute(prepared_write_query, (time, city, country))
+
+def process(rdd):
+    rdd.foreachPartition(lambda record: write_into_cassandra(record))
+
+
 
 
 if __name__ == "__main__":
@@ -36,39 +83,8 @@ if __name__ == "__main__":
     zkQuorum = "52.34.117.127:2181,52.89.22.134:2181,52.35.24.163:2181,52.89.0.97:2181"
     topic = "tweets"
     kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 2})
-    #kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": ['ec2-52-35-24-163.us-west-2.compute.amazonaws.com:9092','ec2-52-89-22-134.us-west-2.compute.amazonaws.com:9092','ec2-52-34-117-127.us-west-2.compute.amazonaws.com:9092','ec2-52-89-0-97.us-west-2.compute.amazonaws.com:9092']})
     lines = kvs.map(lambda x: x[1])
-
-
-    #counts = lines.map(findword) \
-    #    .filter(lambda a: a!=0,) \
-    #    .reduceByKey(add)
-
-
-    def write_into_cassandra(record):
-        from cassandra.cluster import Cluster
-        from cassandra import ConsistencyLevel
-
-        # connect to cassandra
-        cluster = Cluster(['ec2-52-35-24-163.us-west-2.compute.amazonaws.com'])
-        session = cluster.connect("demo")
-
-        prepared_write_query = session.prepare("INSERT INTO demostream (time, city, country) VALUES (?,?,?)")
-        for i in record:
-            #print (i)
-            json_str = json.loads(i)
-            #print (json_str)
-            time = 'ttt'#str(json_str["created_at"])
-            city = 'test'#str(json_str["place"]["name"])
-            country = json_str #str(json_str["text"])
-            session.execute(prepared_write_query, (time, city, country))
-
-
-    def process(rdd):
-        rdd.foreachPartition(lambda record: write_into_cassandra(record))
-
     lines.foreachRDD(process )
-
 
     ssc.start()
     ssc.awaitTermination()
