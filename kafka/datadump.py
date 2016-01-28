fe040730d29d01618dc93cc5e11927b9eeb91eee
Timo Meyer <timomey@gmail.com>
2016-01-27 21:00:17 -0800

    use fake stream

--- kafka/datadump.py
+++ kafka/datadump.py
@@ -5,8 +5,6 @@ import json
 
 
 
-
-
 topic = 'twitterdump_timo'
 cluster = kafka.KafkaClient("ip-172-31-2-200:9092,ip-172-31-2-201:9092,ip-172-31-2-202:9092,ip-172-31-2-203:9092")
 prod = kafka.SimpleProducer(cluster, async = True, batch_send_every_n = 500)
@@ -14,19 +12,17 @@ prod = kafka.SimpleProducer(cluster, async = True, batch_send_every_n = 500)
 
 
 
-with open('lastreadcount.log','r') as log:
-    lastread = log.read()
-    lastcount = int(lastread.strip())
 
-with open('tweets.txt','r') as f:
+with open('tweetsfirstbatch.txt','r') as f:
         counter = 0
         for line in f:
             counter += 1
             try:
                 json_dict = json.loads(line.strip())
                 json_dict['created_at']
+                #print json.dumps(json_dict)
 
-                #tweet = line.strip()
+                tweet = line.strip()
                 prod.send_messages(topic, json.dumps(json_dict))#tweet.encode('utf-8'))
 
             except:
