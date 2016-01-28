1bbe1a8005cc4dadecb499fa47d4274c64062320
Timo Meyer <timomey@gmail.com>
2016-01-27 20:52:47 -0800

    run spark script was pointed to wrong master

--- spark/run_sparkstream.sh
+++ spark/run_sparkstream.sh
@@ -1,1 +1,1 @@
-$SPARK_HOME/bin/spark-submit --executor-memory 10000M --driver-memory 10000M --master spark://ip-172-31-2-200:7077 --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 ./sparkstream.py 
+$SPARK_HOME/bin/spark-submit --executor-memory 10000M --driver-memory 10000M --master spark://ip-172-31-2-199:7077 --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 ./sparkstream.py 
