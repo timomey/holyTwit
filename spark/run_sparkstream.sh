0ec8bf1147eb1e82f20e9a7931d1904758f43787
Timo Meyer <timomey@gmail.com>
2016-01-28 18:41:52 -0800

    add input to run_sparkstreaming

--- spark/run_sparkstream.sh
+++ spark/run_sparkstream.sh
@@ -1,1 +1,3 @@
-$SPARK_HOME/bin/spark-submit --executor-memory 10000M --driver-memory 10000M --master spark://ip-172-31-2-199:7077 --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 ./sparkstream.py 
+#!/bin/bash
+
+$SPARK_HOME/bin/spark-submit --executor-memory 10000M --driver-memory 10000M --master spark://ip-172-31-2-199:7077 --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 ./sparkstream.py bernie 
