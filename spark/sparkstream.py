025bd7f83eada8db812b6a90aca143a6b8a61c0c
Timo Meyer <timomey@gmail.com>
2016-01-28 18:59:27 -0800

    arg parse works - there is no check though

--- spark/sparkstream.py
+++ spark/sparkstream.py
@@ -110,8 +110,7 @@ if __name__ == "__main__":
 
 
     wordofinterest = str(sys.argv[1])
-    print(wordofinterest)
-    sys.exit()
+
     #wordofinterest = 'trump'
 
     sc = SparkContext(appName="TwitterImpact")
