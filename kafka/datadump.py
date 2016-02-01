485e15e124d710cf6d9e1b3a35ba3948aebfc7e4
Timo Meyer <timomey@gmail.com>
2016-02-01 15:41:20 -0800

    back to norml

--- kafka/datadump.py
+++ kafka/datadump.py
@@ -4,15 +4,13 @@ import kafka
 import json
 
 	
-tweetfile = 'filteredtweets.txt'
-
 
 topic = 'twitterdump_timo'
 cluster = kafka.KafkaClient("ip-172-31-2-200:9092,ip-172-31-2-201:9092,ip-172-31-2-202:9092,ip-172-31-2-203:9092")
 prod = kafka.SimpleProducer(cluster, async = True, batch_send_every_t = 1000)
 
 
-with open('../../tweets/'+tweetfile ,'r') as f:
+with open('../../tweets/filteredtweets.txt' ,'r') as f:
 	counter = 0
 	for line in f:
 		counter += 1
