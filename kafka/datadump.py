89a044fddb0dae414e3fe766a395c4a5b09d821a
Timo Meyer <timomey@gmail.com>
2016-02-01 15:16:09 -0800

    indent

--- kafka/datadump.py
+++ kafka/datadump.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
 		tweetdump_filelist[0] = 'filteredtweets.txt'
 
 
-    topic = 'twitterdump_timo'
+	topic = 'twitterdump_timo'
     cluster = kafka.KafkaClient("ip-172-31-2-200:9092,ip-172-31-2-201:9092,ip-172-31-2-202:9092,ip-172-31-2-203:9092")
     prod = kafka.SimpleProducer(cluster, async = True, batch_send_every_t = 500)
 
