4594a77ce9f898b1ca749c852bc78ee9b9a5f8c3
Timo Meyer <timomey@gmail.com>
2016-02-02 19:07:26 -0800

    cassandra frontend query update

--- flask/app/views.py
+++ flask/app/views.py
@@ -31,7 +31,7 @@ def citycount():
 def get_stream(wordofinterest,maxnumpanels):
 
     #maxnumpanels = 10
-    stmt = "SELECT * FROM "+str(wordofinterest)
+    stmt = "SELECT * FROM citycount WHERE wordofinterest="+str(wordofinterest)
 
     response = session.execute(stmt)
     response_list = []
