225ab6f74b57ee298f2d5ce29ee037b4e7cbcb51
Timo Meyer <timomey@gmail.com>
2016-02-03 13:18:28 -0800

    list of interesting words in cassandra now - read when spark starts
    does it also read afterwards again?

--- spark_realtime/citycount.py
+++ spark_realtime/citycount.py
@@ -43,18 +43,24 @@ def clean_string(text):
 def cassandra_create_keyspace(keyspacename,session):
     session.execute("CREATE KEYSPACE IF NOT EXISTS "+keyspacename+" WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor' : 3};")
 
-def cassandra_create_citycount_table(keyspacename, tablename, session):
+def cassandra_create_listofwords_table(keyspacename,session):
+    #list of words meintained in cassandra so that there can be multiple
+    session.execute("CREATE TABLE IF NOT EXISTS "+keyspacename+".listofwords \
+                    (word text, numberofwords int, time timestamp, \
+                    PRIMARY KEY (word, numberofwords));")
+
+def cassandra_create_citycount_table(keyspacename, citycounttablename, session):
     #it not exists create the keyspace
     cassandra_create_keyspace(keyspacename, session)
     # if not exists create table with following schema
-    session.execute("CREATE TABLE IF NOT EXISTS "+keyspacename+"."+tablename+" \
+    session.execute("CREATE TABLE IF NOT EXISTS "+keyspacename+"."+citycounttablename+" \
                         (wordofinterest text, place text, count counter, \
                         PRIMARY KEY (wordofinterest, place)) with clustering order by (place desc); ")
 
-def cassandra_create_citycount_table_readwrite(keyspacename, tablename, session):
+def cassandra_create_citycount_table_readwrite(keyspacename, citycounttablename, session):
     #EQUIVALENT, BUT INSTEAD OF UPDATE, READ AND WRITE -> THIS WAY YOU CAN HAVE A SORTED OUTPUT
     cassandra_create_keyspace(keyspacename, session)
-    session.execute("CREATE TABLE IF NOT EXISTS "+keyspacename+"."+tablename+" \
+    session.execute("CREATE TABLE IF NOT EXISTS "+keyspacename+"."+citycounttablename+" \
                         (wordofinterest text, place text, count int, \
                         PRIMARY KEY ((wordofinterest,place), count)) with clustering order by (count desc); ")
 
@@ -66,18 +72,18 @@ def read_write_to_cassandra(record):
         'ec2-52-35-98-229.us-west-2.compute.amazonaws.com',
         'ec2-52-34-216-192.us-west-2.compute.amazonaws.com'])
     session = cluster.connect()
-    cassandra_create_citycount_table_readwrite(keyspacename,tablename, session)
-    prepared_write_query = session.prepare("INSERT INTO "+keyspacename+"."+tablename+" (wordofinterest,place,count) VALUES (?,?,?) ")
+    cassandra_create_citycount_table_readwrite(keyspacename,citycounttablename, session)
+    prepared_write_query = session.prepare("INSERT INTO "+keyspacename+"."+citycounttablename+" (wordofinterest,place,count) VALUES (?,?,?) ")
     for element in record:
         place = str(element[0][0])+", "+ str(element[0][1])
         count = element[1]
-        read_stmt = "select count from "+keyspacename+"."+tablename+" where wordofinterest='"+wordofinterest+"' and place='"+place+"';"
+        read_stmt = "select count from "+keyspacename+"."+citycounttablename+" where wordofinterest='"+wordofinterest+"' and place='"+place+"';"
         response = session.execute(read_stmt)
         oldcount =0
         if len(response[:]) >0:
             for res in response:
                 oldcount += res.count
-            session.execute("delete from "+keyspacename+"."+tablename+" where wordofinterest='"+wordofinterest+"' and place='"+place+"';")
+            session.execute("delete from "+keyspacename+"."+citycounttablename+" where wordofinterest='"+wordofinterest+"' and place='"+place+"';")
         else:
             oldcount=0
         session.execute(prepared_write_query, (wordofinterest, place, count+oldcount))
@@ -91,16 +97,13 @@ def update_to_cassandra(record):
         'ec2-52-35-98-229.us-west-2.compute.amazonaws.com',
         'ec2-52-34-216-192.us-west-2.compute.amazonaws.com'])
     session = cluster.connect()
-    cassandra_create_citycount_table(keyspacename,tablename, session)
+    cassandra_create_citycount_table(keyspacename,citycounttablename, session)
 
-    prepared_write_query = session.prepare("UPDATE "+keyspacename+"."+tablename+" SET count = count + ? WHERE place=? AND wordofinterest=?")
+    prepared_write_query = session.prepare("UPDATE "+keyspacename+"."+citycounttablename+" SET count = count + ? WHERE place=? AND wordofinterest=?")
     for element in record:
         place = str(element[0][0])+", "+ str(element[0][1])
         count = element[1]
         session.execute(prepared_write_query, (count, place, wordofinterest))
-
-
-
 
 
 def citycount_to_cassandra(rdd):
@@ -108,17 +111,11 @@ def citycount_to_cassandra(rdd):
     #so for each partition, do what you wanna do ->
     rdd.foreachPartition(lambda record: update_to_cassandra(record))
 
-
-
-
-
 if __name__ == "__main__":
-
-    wordofinterest = str(sys.argv[1])
-
-    #cassandra keyspace name
+    #wordofinterest = str(sys.argv[1])
+    #cassandra keyspace/table names
     keyspacename = 'holytwit'
-    tablename = 'citycount'
+    citycounttablename = 'citycount'
 
     #spark streaming objects
     sc = SparkContext(appName="TwitterImpact")
@@ -132,20 +129,32 @@ if __name__ == "__main__":
     kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 4})
     lines = kvs.map(lambda x: x[1])
 
-    #1. filter: is the word in the tweet. 2.filter does it have a place name 3. filter does it have country country_code
-    #4. map it to ((place.name, place.country_code),1).
-    #5. reducebykey add a+b -> sum for each place.
-    #def countcity(lines):
-    output = lines.filter(lambda l: wordofinterest in json.loads(l)["text"])\
-        .filter(lambda l: len(json.loads(l)["place"]["name"]) > 0 )\
-        .filter(lambda l: len(json.loads(l)["place"]["country_code"]) > 0)\
-        .filter(lambda l: len(json.loads(l)["timestamp_ms"]) >0  )\
-        .map(lambda l: ( (json.loads(l)["place"]["name"], json.loads(l)["place"]["country_code"] ), 1))\
-        .reduceByKey(lambda a,b: a+b)
+    #get wordlist
+    cluster = Cluster([
+        'ec2-52-89-218-166.us-west-2.compute.amazonaws.com',
+        'ec2-52-88-157-153.us-west-2.compute.amazonaws.com',
+        'ec2-52-35-98-229.us-west-2.compute.amazonaws.com',
+        'ec2-52-34-216-192.us-west-2.compute.amazonaws.com'])
+    session = cluster.connect()
 
-    #before doing the stuff, create the table if necessary (schema defined here too)
-    #output is a DStream object containing a bunch of RDDs. for each rdd go ->
-    output.foreachRDD(citycount_to_cassandra)
+    read_stmt = "select word,numberofwords from "+keyspacename+".listofwords ;"
+    response = session.execute(read_stmt)
+    wordlist = [row.word for row in response]
+
+    for wordofinterest in wordlist:
+        #1. filter: is the word in the tweet. 2.filter does it have a place name 3. filter does it have country country_code
+        #4. map it to ((place.name, place.country_code),1).
+        #5. reducebykey add a+b -> sum for each place.
+        #def countcity(lines):
+        output = lines.filter(lambda l: wordofinterest in json.loads(l)["text"])\
+            .filter(lambda l: len(json.loads(l)["place"]["name"]) > 0 )\
+            .filter(lambda l: len(json.loads(l)["place"]["country_code"]) > 0)\
+            .filter(lambda l: len(json.loads(l)["timestamp_ms"]) >0  )\
+            .map(lambda l: ( (json.loads(l)["place"]["name"], json.loads(l)["place"]["country_code"] ), 1))\
+            .reduceByKey(lambda a,b: a+b)
+        #before doing the stuff, create the table if necessary (schema defined here too)
+        #output is a DStream object containing a bunch of RDDs. for each rdd go ->
+        output.foreachRDD(citycount_to_cassandra)
 
     #start the stream and keep it running - await for termination too.
     ssc.start()
--- spark_realtime/run_citycount.sh
+++ spark_realtime/run_citycount.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-    echo "dude, need a word of input here" && exit 1
+if [ "$#" -ne 0 ]; then
+    echo "dude, use the webinterface to input your word" && exit 1
 fi
 
-WORDOFINTEREST=$1
 
-$SPARK_HOME/bin/spark-submit --executor-memory 10000M --driver-memory 10000M --master spark://ip-172-31-2-199:7077 --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 ./citycount.py $WORDOFINTEREST
+$SPARK_HOME/bin/spark-submit --executor-memory 10000M --driver-memory 10000M --master spark://ip-172-31-2-199:7077 --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 ./citycount.py 
