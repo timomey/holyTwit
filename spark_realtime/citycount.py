8960673d8b8e7011a8144512881f5f3a9fd517a9
Timo Meyer <timomey@gmail.com>
2016-02-03 20:59:18 -0800

    persist

--- spark_realtime/citycount.py
+++ spark_realtime/citycount.py
@@ -144,7 +144,7 @@ if __name__ == "__main__":
     kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 4})
     lines = kvs.map(lambda x: x[1])
 
-    lines.persist(StorageLevel.MEMORY_AND_DISK)
+    lines.MEMORY_AND_DISK()
     for wordofinterest in wordlist:
         #1. filter: is the word in the tweet. 2.filter does it have a place name 3. filter does it have country country_code
         #4. map it to ((place.name, place.country_code),1).
