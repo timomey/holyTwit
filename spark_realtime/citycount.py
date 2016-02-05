a321535137e40d897a4ff25fb8d85ba25618f37c
Timo Meyer <timomey@gmail.com>
2016-02-04 17:14:47 -0800

    data is in tuple with 3 elements

--- spark_realtime/citycount.py
+++ spark_realtime/citycount.py
@@ -101,9 +101,10 @@ def update_to_cassandra(record):
 
     prepared_write_query = session.prepare("UPDATE "+keyspacename+"."+citycounttablename+" SET count = count + ? WHERE place=? AND wordofinterest=? ")
     for element in record:
-        place = str(element[0][0].encode('ascii','ignore'))+", "+ str(element[0][1].encode('ascii','ignore'))
-        count = element[1]
-        session.execute(prepared_write_query, (count, place, wordofinterest))
+        word = element[0][0]
+        place = str(element[0][1].encode('ascii','ignore'))+", "+ str(element[0][2].encode('ascii','ignore'))
+        count = element[2]
+        session.execute(prepared_write_query, (count, place, word))
 
 
 def citycount_to_cassandra(rdd):
