f6dbbb01e90babdc5f4c22e8f5c645cc6c3bf79c
Timo Meyer <timomey@gmail.com>
2016-02-04 19:53:38 -0800

    qf

--- spark_realtime/citycount.py
+++ spark_realtime/citycount.py
@@ -167,7 +167,8 @@ if __name__ == "__main__":
     #output = lines.filter(lambda l: wordofinterest in json.loads(l)["text"])\
     #output = lines.map(lambda l: json.loads(l)["place"]["name"] )
     output = lines.filter(lambda l: len(json.loads(l)['text'])>0 )\
-        .filter(lambda l: len(json.loads(l)["place"]["country_code"]) > 0)
+        .filter(lambda l: len(json.loads(l)["place"]["country_code"]) > 0)\
+        .filter(lambda l: len(json.loads(l)["place"]["name"])>0 )
         #.flatMap(lambda l: lambda_map_word_city2(l) )\
         #.filter(lambda l: not not l)
         #.reduceByKey(lambda a,b: a+b)
