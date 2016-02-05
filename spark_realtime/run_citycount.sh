300afff6bc363a4faffdda47624f29ee683d3210
Timo Meyer <timomey@gmail.com>
2016-02-05 09:39:23 -0800

    more memory for spark jobs

--- spark_realtime/run_citycount.sh
+++ spark_realtime/run_citycount.sh
@@ -5,4 +5,4 @@ if [ "$#" -ne 0 ]; then
 fi
 
 
-$SPARK_HOME/bin/spark-submit --executor-memory 10000M --driver-memory 10000M --master spark://ip-172-31-2-199:7077 --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 ./citycount.py 
+$SPARK_HOME/bin/spark-submit --executor-memory 14000M --driver-memory 14000M --master spark://ip-172-31-2-199:7077 --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 ./citycount.py 
--- spark_realtime/run_topicgraph.sh
+++ spark_realtime/run_topicgraph.sh
@@ -4,4 +4,4 @@ if [ "$#" -ne 0 ]; then
     echo "dude, need a word of input here" && exit 1
 fi
 
-$SPARK_HOME/bin/spark-submit --executor-memory 10000M --driver-memory 10000M --master spark://ip-172-31-2-199:7077 --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 ./topicgraph.py 
+$SPARK_HOME/bin/spark-submit --executor-memory 14000M --driver-memory 14000M --master spark://ip-172-31-2-199:7077 --packages org.apache.spark:spark-streaming-kafka_2.10:1.6.0 ./topicgraph.py 
