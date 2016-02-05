95b98a83d8cadcb294c9da8eba9c3361e350a2ab
Timo Meyer <timomey@gmail.com>
2016-02-04 20:13:52 -0800

    qf

--- spark_realtime/citycount.py
+++ spark_realtime/citycount.py
@@ -169,7 +169,7 @@ if __name__ == "__main__":
     output = lines.filter(lambda l: len(json.loads(l)['text'])>0 )\
         .filter(lambda l: len(json.loads(l)["place"]["country_code"]) > 0)\
         .filter(lambda l: len(json.loads(l)["place"]["name"])>0 )\
-        .flatMap(lambda l: lambda_map_word_city2(l) )\
+        .flatMap(lambda l: lambda_map_word_city(l) )\
         .reduceByKey(lambda a,b: a+b)
         #.filter(lambda l: len(json.loads(l)['text'])>0)
     output.foreachRDD(citycount_to_cassandra)
