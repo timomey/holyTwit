f0230e5fa1bdd4e1180c38080a1bab34cddaf0fe
Timo Meyer <timomey@gmail.com>
2016-02-06 16:38:30 -0800

    qf

--- flask/app/views.py
+++ flask/app/views.py
@@ -9,9 +9,9 @@ import time
 
 # App config.
 #DEBUG = True
-app = Flask(__name__)
-app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a123'
+#app = Flask(__name__)
+#app.config.from_object(__name__)
+#app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 
 
