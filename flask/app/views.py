12086d308690d8c984a8a50b937093cd17fd29d2
Timo Meyer <timomey@gmail.com>
2016-02-06 16:18:05 -0800

    flasjk update key

--- flask/app/views.py
+++ flask/app/views.py
@@ -7,6 +7,12 @@ from subprocess import call
 import time
 
 
+# App config.
+DEBUG = True
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a123'
+
 
 cluster = Cluster(['ec2-52-89-218-166.us-west-2.compute.amazonaws.com','ec2-52-88-157-153.us-west-2.compute.amazonaws.com','ec2-52-35-98-229.us-west-2.compute.amazonaws.com','ec2-52-34-216-192.us-west-2.compute.amazonaws.com'])
 session = cluster.connect('twitterimpact')
