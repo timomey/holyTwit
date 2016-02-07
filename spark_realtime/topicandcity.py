d608b98c9fbe37d502e0b01baa1f470e4c34dc09
Timo Meyer <timomey@gmail.com>
2016-02-07 13:36:50 -0800

    durn the whole machine on!

--- spark_realtime/topicandcity.py
+++ spark_realtime/topicandcity.py
@@ -196,9 +196,9 @@ if __name__ == "__main__":
                             return_list_of_tuples.append( ( (word_input, str(word_tweet.encode('ascii','ignore')), tuuple[1] ) , 1) )
                             return  return_list_of_tuples
                         else:
-                            return [((word_input,'skip'),1)]
+                            return [((word_input,'intweet'),1)]
                 else:
-                    return [((word_input,'not in tweet'),1)]
+                    return [((word_input,'notintweet'),1)]
         else:
             return [tuuple]
 
@@ -216,8 +216,8 @@ if __name__ == "__main__":
         .reduceByKey(lambda a,b: a+b)
         #.map(lambda l: (l[1],l[0]))\
         #.transform(sortByKey)
-    output.pprint()
-    #output.foreachRDD(topicgraph_to_cassandra)
+    #output.pprint()
+    output.foreachRDD(topicgraph_to_cassandra)
 
 
     def lambda_map_word_city(tweet):
