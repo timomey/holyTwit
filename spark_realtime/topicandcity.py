b37bb3159ac73391f8d7e81bf70c825137998b91
Timo Meyer <timomey@gmail.com>
2016-02-07 11:55:53 -0800

    flatmap wrong line?!?

--- spark_realtime/topicandcity.py
+++ spark_realtime/topicandcity.py
@@ -207,7 +207,8 @@ if __name__ == "__main__":
             return (('error','error'),0)
 
     output = lines.map(lambda l: textsplit_placetuple ) \
-        .flatMap(lambda l: lambda_map_word_connections(l)) \
+        .map(lambda l: lambda_map_word_connections(l)) \
+        .flatMap(lambda l: l)\
         .reduceByKey(lambda a,b: a+b)
         #.map(lambda l: (l[1],l[0]))\
         #.transform(sortByKey)
