76fbe2083496b19ada8ec69d90d84870428b855c
Timo Meyer <timomey@gmail.com>
2016-02-08 12:20:25 -0800

    change streaming.py - web -> stream -> ES

--- spark_realtime/streaming.py
+++ spark_realtime/streaming.py
@@ -31,73 +31,66 @@ if __name__ == "__main__":
     sc = SparkContext(appName="topicgraph")
     ssc = StreamingContext(sc, 1)
 
+
+    number_queries = sc.accumulator(1)
+
     #ELASTICSEARCH STUFF
+    es = Elasticsearch(hosts=[{"host":"52.34.117.127", "port":9200},{"host":"52.89.22.134", "port":9200},{"host":"52.35.24.163", "port":9200},{"host":"52.89.0.97", "port":9200}] )
+    es.indices.delete(index='twit', ignore=400)
     # index and document type constants
     #create index for ES (ignore if it already exists       )
-    #es.indices.create(index='twit', ignore=400, body={
-    #      "mappings": {
-    #        "document": {
-    #          "properties": {
-    #            "message": {
-    #              "type": "string"
-    #            }
-    #          }
-    #        }
-    #      }
-    #    }
-    #)
+    es.indices.create(index='twit', ignore=400, body={
+          "mappings": {
+            "document": {
+              "properties": {
+                "message": {
+                  "type": "string"
+                }
+              }
+            }
+          }
+        }
+    )
 
     #zookeeper quorum for to connect to kafka (local ips for faster access)
     zkQuorum = "52.34.117.127:2181,52.89.22.134:2181,52.35.24.163:2181,52.89.0.97:2181"
     #brokers = "52.34.117.127:9092,52.89.22.134:9092,52.35.24.163:9092,52.89.0.97:9092"
     #kafka topic to consume from:
     topic = "faketwitterstream"
+
     #alternative kafka stream:
     #directKafkaStream = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
     #StorageLevel.MEMORY_AND_DISK_SER
+
     #kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-topicgraph", {topic: 8})
     #2nd stream for search querries
     kquerys = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-elastc", {"elasticquerries": 8})
     userqueries = kquerys.map(lambda x: x[1])
     #lines = kvs.map(lambda x: x[1])
 
-    es = Elasticsearch(hosts=[{"host":["52.34.117.127","52.89.22.134","52.35.24.163","52.89.0.97"], "port":9200}] )
 
-    def query_to_es(iter):
-        INDEX_NAME = "twit"
-        TYPE = "document"
-
-        #elastic search connection:
-        es = Elasticsearch(hosts=[{"host":["52.34.117.127","52.89.22.134","52.35.24.163","52.89.0.97"], "port":9200}] )
-        if iter:
-            count=0
-            for word in iter:
-                #wordlist = inpu.split()
-                count+=1
-                es.create(index='twit', doc_type='.percolator', body={'query': {'match': {'message': word  }}}, id=count)
 
     def eachrddfct(rdd):
         rdd.foreachPartition(lambda record: query_to_es(record) )
 
     def sendPartition(iter):
-        es = Elasticsearch(hosts=[{"host":["52.34.117.127","52.89.22.134","52.35.24.163","52.89.0.97"], "port":9200}] )
-        count =0
+        es = Elasticsearch(hosts=[{"host":"52.34.117.127", "port":9200},{"host":"52.89.22.134", "port":9200},{"host":"52.35.24.163", "port":9200},{"host":"52.89.0.97", "port":9200}] )
         try:
             for word in iter:
-                count+=1
-                es.create(index='twit', doc_type='.percolator', body={'query': {'match': {'message': word  }}}, id=count)
-        except TypeError:
+                es.create(index='twit', doc_type='.percolator', body={'query': {'match': {'message': word  }}}, id=number_queries.value)
+                number_queries.add(1)
+        except TypeError:#this exception handles the ongoing empty stream. TypeError: NoneType
             pass
 
 
     inputwords = userqueries.flatMap(lambda l: str(l).split() )
     inputwords.foreachRDD(lambda rdd: rdd.foreachPartition(sendPartition ))
-
+    inputwords.pprint()
     ########################get all id's
-    #res = es.search(
-    #    index="twit",
-    #    body={"query": {"match_all": {}}, "size": 30000, "fields": ["_id"]})
-    #ids = [d['_id'] for d in res['hits']['hits']]
+    #    res = es.search(
+    #        index="twit",
+    #        body={"query": {"match_all": {}}, "size": 30000, "fields": ["_id"]})
+    #    ids = [d['_id'] for d in res['hits']['hits']]
     #start the stream and keep it running - await for termination too.
     ########################
     ssc.start()
