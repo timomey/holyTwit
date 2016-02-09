88b96f1d0e8ed17cecbf8d4971ff6db1538c928e
Timo Meyer <timomey@gmail.com>
2016-02-08 16:34:39 -0800

    topicandcity clean up to test

--- spark_realtime/topicandcity.py
+++ spark_realtime/topicandcity.py
@@ -127,27 +127,6 @@ if __name__ == "__main__":
     sc = SparkContext(appName="topicgraph")
     ssc = StreamingContext(sc, 1)
 
-    #ELASTICSEARCH STUFF
-    # index and document type constants
-    INDEX_NAME = "documents"
-    TYPE = "document"
-
-    #es = Elasticsearch(hosts=[{"host":["52.34.117.127","52.89.22.134","52.35.24.163","52.89.0.97"], "port":9200}])
-
-    #es.indices.create(index='twit', ignore=400, body={
-    #      "mappings": {
-    #        "document": {
-    #          "properties": {
-    #            "message": {
-    #              "type": "string"
-    #            }
-    #          }
-    #        }
-    #      }
-    #    }
-    #)
-
-
     #zookeeper quorum for to connect to kafka (local ips for faster access)
     zkQuorum = "52.34.117.127:2181,52.89.22.134:2181,52.35.24.163:2181,52.89.0.97:2181"
     #brokers = "52.34.117.127:9092,52.89.22.134:9092,52.35.24.163:9092,52.89.0.97:9092"
@@ -157,7 +136,6 @@ if __name__ == "__main__":
     #directKafkaStream = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
     kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-topicgraph", {topic: 8})
     #2nd stream for search querries
-    kquerys = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-topicgraph", {"elasticquerries": 8})
     userqueries = kquerys.map(lambda x: x[1])
     lines = kvs.map(lambda x: x[1])
 
