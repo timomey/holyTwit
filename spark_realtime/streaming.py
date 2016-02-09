8e806b90d91bb345b62aade5b2d72821b4805972
Timo Meyer <timomey@gmail.com>
2016-02-08 16:25:54 -0800

    streaming: ES percolate check return list of tuple and reducebykey; pprint check

--- spark_realtime/streaming.py
+++ spark_realtime/streaming.py
@@ -66,6 +66,40 @@ if __name__ == "__main__":
     ############################################
     # twitterstream ->
     ############################################
+    #ES connection on the driver level
+    es = Elasticsearch(hosts=[{"host":"52.34.117.127", "port":9200},{"host":"52.89.22.134", "port":9200},{"host":"52.35.24.163", "port":9200},{"host":"52.89.0.97", "port":9200}] )
+
+    def text_hashtags_place_tuple(tweet):
+        return_list_of_tuples=[]
+        #see if the text is there:
+        try:
+            text = json.loads(tweet)["text"]
+        except TypeError:
+            return [(('notext','na','na'),1)]
+        #check if this text contains any of the keywords.
+        esresult = es.percolate(index='twit',doc_type='.percolate', body={'doc':{'message': text }})
+        if esresult['matches']:
+            matched_words = []
+            #for all matches
+            for match in esresult['matches']:
+                #get the matched keyword and store in list
+                perc_match = es.get(index='twit',doc_type='.percolator',id=match['_id'])
+                matched_words.append(perc_match['_source']['query']['match']['message'])
+            #get hashtags and place
+            try:
+                hashtags = [hash.split()[0] for hash in text.split('#')[1:]]
+                place = str( json.loads(tweet)["place"]["name"].encode('ascii','ignore'))+","+str(json.loads(tweet)["place"]["country_code"].encode('ascii','ignore') )
+                list_of_lists_of_tuples = map(lambda x: [(x,ht,place) for ht in hashtags] ,matched_words)
+                list_of_tuple = [(item,1) for sublist in list_of_lists_of_tuples for item in sublist]
+                return list_of_tuple
+            except IndexError:
+                return [((keywords, 'nohashtags', place),1) for keywords in matched_words]
+            except: TypeError:
+                list_of_lists_of_tuples = map(lambda x: [(x,ht,'noplace') for ht in hashtags] ,matched_words)
+                list_of_tuple = [(item,1) for sublist in list_of_lists_of_tuples for item in sublist]
+                return list_of_tuple
+
+
 
     hashtagsoutput = lines.map(lambda l: text_hashtags_place_tuple(l) )\
         .flatMap(lambda l: l)\
--- spark_realtime/topicandcity.py
+++ spark_realtime/topicandcity.py
@@ -211,27 +211,6 @@ if __name__ == "__main__":
     #output.foreachRDD(topicgraph_to_cassandra)
 
 
-    def text_hashtags_place_tuple(tweet):
-        return_list_of_tuples=[]
-        try:
-            text = json.loads(tweet)["text"]
-        except TypeError:
-            return [(('notext','na','na'),1)]
-        for word_input in wordlist:
-            if word_input in text:
-                #hashtags:
-                try:
-                    hashtags = [hash.split()[0] for hash in text.split('#')[1:]]
-                    place = str(json.loads(tweet)["place"]["name"].encode('ascii','ignore')+","+json.loads(tweet)["place"]["country_code"].encode('ascii','ignore'))
-                    for ht in hashtags:
-                        return_list_of_tuples.append( ((word_input ,ht, place),1) )
-                    return return_list_of_tuples
-                except IndexError:
-                    return [((word_input, 'nohashtags', place),1)]
-                except TypeError:
-                    return [((word_input, 'ht', 'noplace'),1)]
-            else:
-                return [((word_input, 'notintweet', 'na'),1)]
 
     hashtagsoutput = lines.map(lambda l: text_hashtags_place_tuple(l) )\
         .flatMap(lambda l: l)\
