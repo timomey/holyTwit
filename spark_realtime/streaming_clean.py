9a43908839efc88fa35d560b13727de622940017
Timo Meyer <timomey@gmail.com>
2016-02-11 10:07:16 -0800

    slow down

--- spark_realtime/streaming_clean.py
+++ spark_realtime/streaming_clean.py
@@ -103,7 +103,7 @@ if __name__ == "__main__":
     conf = SparkConf()
     conf.setAppName("holyTwit")
     conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-    conf.set("spark.streaming.receiver.maxRate", 500)
+    conf.set("spark.streaming.receiver.maxRate", 50)
     sc = SparkContext(conf=conf)
     ssc = StreamingContext(sc, 2)
 
