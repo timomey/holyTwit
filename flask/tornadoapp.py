d9e148d0671b138ee5f87422099a17933b4d3332
Timo Meyer <timomey@gmail.com>
2016-02-16 19:37:15 -0800

    add secret key

--- flask/tornadoapp.py
+++ flask/tornadoapp.py
@@ -5,6 +5,10 @@ from tornado.ioloop import IOLoop
 from tornado.web import FallbackHandler, RequestHandler, Application
 from app import app
 
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a123'
+app.run(host='0.0.0.0', debug = True)
+
 class MainHandler(RequestHandler):
 	def get(self):
 		self.write("This message comes from Tornado ^_^")
