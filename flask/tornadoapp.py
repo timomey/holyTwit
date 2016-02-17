056f8d6112a5d8021c5709e7d849e07fe582537d
Timo Meyer <timomey@gmail.com>
2016-02-16 19:41:35 -0800

    set sec. key in main

--- flask/tornadoapp.py
+++ flask/tornadoapp.py
@@ -5,9 +5,6 @@ from tornado.ioloop import IOLoop
 from tornado.web import FallbackHandler, RequestHandler, Application
 from app import app
 
-app.config.from_object(__name__)
-app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a123'
-app.run(host='0.0.0.0', debug = True)
 
 class MainHandler(RequestHandler):
 	def get(self):
@@ -21,5 +18,8 @@ class MainHandler(RequestHandler):
 				])
 
 		if __name__ == "__main__":
+			app.config.from_object(__name__)
+			app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a123'
+			app.run(host='0.0.0.0', debug = True)
 			application.listen(80)
 			IOLoop.instance().start()
