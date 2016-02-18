c0e05cf239009b3e135c22ea0afe36853ae49d6a
Timo Meyer <timomey@gmail.com>
2016-02-18 09:52:00 -0800

    get current words from file instead of ES

--- flask/app/views.py
+++ flask/app/views.py
@@ -187,9 +187,13 @@ def citycount():
 @app.route('/output/')
 def get_stream():
     #get the words from ES
-    es = Elasticsearch(hosts=[{"host":"ip-172-31-2-202", "port":9200},{"host":"ip-172-31-2-201", "port":9200},{"host":"ip-172-31-2-200", "port":9200},{"host":"ip-172-31-2-203", "port":9200}] )
-    pers = es.search(index='twit',doc_type='.percolator')
-    listof_words_in_es = map(lambda x: str(x['_source']['query']['match']['message']), pers['hits']['hits'])
+    #es = Elasticsearch(hosts=[{"host":"ip-172-31-2-202", "port":9200},{"host":"ip-172-31-2-201", "port":9200},{"host":"ip-172-31-2-200", "port":9200},{"host":"ip-172-31-2-203", "port":9200}] )
+    #pers = es.search(index='twit',doc_type='.percolator')
+    #listof_words_in_es = map(lambda x: str(x['_source']['query']['match']['message']), pers['hits']['hits'])
+    listof_words_in_es = []
+    with open('words.txt','r') as words:
+        for line in words:
+            listof_words_in_es.append(line.strip())
     #Cassandra connection:
     cluster = Cluster([
         'ec2-52-36-123-77.us-west-2.compute.amazonaws.com',
