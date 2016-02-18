e6f0442f94d51e151f24b66b30b4363d6d93cb57
Timo Meyer <timomey@gmail.com>
2016-02-18 10:15:31 -0800

    add drilldown

--- flask/app/views.py
+++ flask/app/views.py
@@ -218,20 +218,19 @@ def get_stream():
 
         response_places = [ {'name': str(x.place), 'y': x.count, 'drilldown': 'null' } for x in response_list]
         placesdata[words+'place'] = response_places
-        response_hashtags_list = [ {'name': str(x.degree1), 'y': x.count, 'drilldown': 'null'} for x in response_hashtags_list ]
-        hashtagdata[words+'connection'] = response_hashtags_list
+        response_hashtags_list_data = [ {'name': str(x.degree1), 'y': x.count, 'drilldown': str(x.degree1)} for x in response_hashtags_list ]
+        hashtagdata[words+'connection'] = response_hashtags_list_data
 
-        #top10_connections = [x.degree1 for x in response_hashtags_list if x.count > 2]
-        #send the top10 connections back to ELASTICSEARCH -> DONE BY EXTERNAL script
-        #deg2_visuals = []
-        #for deg1 in top10_connections:
-        #    deg2 = "SELECT count,degree1 FROM holytwit.highestconnection WHERE word='"+str(deg1)+"' LIMIT 10;"
-        #    response_deg2 = session.execute(deg2)
-        #    response_deg2_list =[]
-        #    for val in response_deg2:
-        #        response_deg2_list.append(val)
-        #    drilldown_data = [[str(x.degree1), x.count] for x in response_deg2_list]
-        #    deg2_visuals.append({'name': str(x.degree1), 'id': str(x.degree1), 'data': drilldown_data})
+        top10_connections = [x.degree1 for x in response_hashtags_list ]
+        deg2_visuals = []
+        for deg1 in top10_connections:
+            deg2 = "SELECT count,degree1 FROM holytwit.highestconnection WHERE word='"+str(deg1)+"' LIMIT 10;"
+            response_deg2 = session.execute(deg2)
+            response_deg2_list =[]
+            for val in response_deg2:
+                response_deg2_list.append(val)
+            drilldown_data = [[str(x.degree1), x.count] for x in response_deg2_list]
+            deg2_visuals.append({'name': deg1, 'id': deg1, 'data': drilldown_data})
         #put all deg2_visuals into one dictionary
-        #deg2_visuals_dict[words+'deg2'] = deg2_visuals
-    return render_template("output.html", data_places = placesdata, data_hashtags = hashtagdata, list_of_words = listof_words_in_es)
+        deg2_visuals_dict[words+'deg2'] = deg2_visuals
+    return render_template("output.html", data_places = placesdata, data_hashtags = hashtagdata, list_of_words = listof_words_in_es, data_deg2 = deg2_visuals_dict)
