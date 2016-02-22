328107c2df7d3169f760520802c6dcc57f082501
Timo Meyer <timomey@gmail.com>
2016-02-21 22:59:07 -0800

    infinit datadump

--- kafka/datadump_f2.py
+++ kafka/datadump_f2.py
@@ -19,22 +19,23 @@ if __name__ == "__main__":
 	cluster = kafka.KafkaClient("ip-172-31-2-200:9092,ip-172-31-2-201:9092,ip-172-31-2-202:9092,ip-172-31-2-203:9092")
 	prod = kafka.SimpleProducer(cluster, async = True, batch_send_every_n = 1000)
 
-	for tweetfile in tweetdump_filelist:
+	while True:
+		for tweetfile in tweetdump_filelist:
 
-		with open('../../tweets/'+tweetfile ,'r') as f:
-			counter = 0
-			for line in f:
-				counter += 1
-				#try:
-				json_dict = json.loads(line.strip())
-				#json_dict['created_at']
-				#print json.dumps(json_dict)
+			with open('../../tweets/'+tweetfile ,'r') as f:
+				counter = 0
+				for line in f:
+					counter += 1
+					#try:
+					json_dict = json.loads(line.strip())
+					#json_dict['created_at']
+					#print json.dumps(json_dict)
 
-				#tweet = line.strip()
-				prod.send_messages(topic, json.dumps(json_dict))#tweet.encode('utf-8'))
+					#tweet = line.strip()
+					prod.send_messages(topic, json.dumps(json_dict))#tweet.encode('utf-8'))
 
-				#except:
-				#    continue
+					#except:
+					#    continue
 
-				if counter%10000 ==0:
-					print counter
+					if counter%10000 ==0:
+						print counter
