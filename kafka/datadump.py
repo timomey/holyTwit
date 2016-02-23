d0818a1f9cf1c203b907e332ee3bf96cbd053683
Timo Meyer <timomey@gmail.com>
2016-02-23 14:24:28 -0800

    change artificial datadump to slow infinit stream

--- kafka/datadump.py
+++ kafka/datadump.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
+#this script creates a inifinit SLOW stream.
 import kafka
 import json
 import sys
+import time
 
-if __name__ == "__main__":
+if __name_	time.sleep(1) == "__main__":
 
 	tweetdump_filelist = list()
 	if len(sys.argv) >1:
@@ -13,31 +15,25 @@ if __name__ == "__main__":
 			tweetdump_filelist.append(str(sys.argv[i]))
 	else:
 		tweetdump_filelist.append('clean2_2016-02-08-11-57_tweets.txt')
-		tweetdump_filelist.append('clean2_2016-02-08-11-57_tweets.txt')
-		tweetdump_filelist.append('clean2_2016-02-08-11-57_tweets.txt')
-		tweetdump_filelist.append('clean2_2016-02-08-11-57_tweets.txt')
 
 
 	topic = 'faketwitterstream'
 	cluster = kafka.KafkaClient("ip-172-31-2-200:9092,ip-172-31-2-201:9092,ip-172-31-2-202:9092,ip-172-31-2-203:9092")
 	prod = kafka.SimpleProducer(cluster, async = True, batch_send_every_n = 1000)
 
-	for tweetfile in tweetdump_filelist:
+	while True:
+		for tweetfile in tweetdump_filelist:
 
-		with open('../../tweets/'+tweetfile ,'r') as f:
-			counter = 0
-			for line in f:
-				counter += 1
-				#try:
-				json_dict = json.loads(line.strip())
-				#json_dict['created_at']
-				#print json.dumps(json_dict)
+			with open('../../tweets/'+tweetfile ,'r') as f:
+				counter = 0
+				for line in f:
+					counter += 1
+					json_dict = json.loads(line.strip())
+					prod.send_messages(topic, json.dumps(json_dict))
 
-				#tweet = line.strip()
-				prod.send_messages(topic, json.dumps(json_dict))#tweet.encode('utf-8'))
+					#
+					if counter % 50 ==0:
+						time.sleep(1)
 
-				#except:
-				#    continue
-
-				if counter%10000 ==0:
-					print counter
+					if counter%10000 ==0:
+						print counter
